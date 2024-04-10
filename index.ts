4a892bf02e879c973b417a6886ce7963d342e76f
muhammmadhasnain <166405940+muhammmadhasnain@users.noreply.github.com>
2024-04-09 20:15:54 -0700

    push cli  gass number  game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /urs/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1)
+
+const gassNumber = await inquirer.prompt([
+    {
+        message: "Enter Your Number",
+        type: "number",
+        name: "GassNumber"
+    }
+])
+
+if(gassNumber.GassNumber === randomNumber){
+    console.log("congratulation you win")
+}else{
+    console.log("you lose")
+}
